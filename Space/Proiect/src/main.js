f1ebffe4b1b0c891ef76eb14a8a8e090d21bb951
Ion <prc_ion@outlook.com>
2020-12-29 14:38:44 +0200

    implement camera rotation

--- Space/Proiect/src/main.js
+++ Space/Proiect/src/main.js
@@ -386,6 +386,34 @@ function display(currentTime) {
 
   requestAnimationFrame(display)
 }
+function keyDownHandler(event) {
+  var a = myCamera.location;
+  if (event.keyCode == 39 || event.keyCode == 68) {
+    myCamera.location = [a[0] + 0.2, a[1], a[2]];
+  }
+
+  if (event.keyCode == 37 || event.keyCode == 65) {
+    myCamera.location = [a[0] - 0.2, a[1], a[2]];
+  }
+
+  if (event.keyCode == 38 || event.keyCode == 87) {
+    myCamera.location = [a[0], a[1], a[2] + 0.2];
+  }
+
+  if (event.keyCode == 40 || event.keyCode == 83) {
+    myCamera.location = [a[0], a[1], a[2] + -0.2];
+  }
+}
+
+function mouseMoveHandler(event) {
+  var a = myCamera.location;
+  var witdh = gl.canvas.clientWidth;
+  var height = gl.canvas.clientHeight;
+  const factor = 2;
+  const xpos = (event.offsetX - witdh / 2) / witdh
+  const ypos = -(event.offsetY - height / 2) / height
+  myCamera.location = [factor * xpos, factor * ypos, a[2]]
+}
 
 function main() {
   const canvas = document.querySelector('#glcanvas')
@@ -396,9 +424,8 @@ function main() {
     alert('Inițializare WebGL eșuată.')
     return
   }
-
+  document.addEventListener('keydown', keyDownHandler)
+  canvas.addEventListener('mousemove', mouseMoveHandler)
   init()
   display()
 }
-
-
